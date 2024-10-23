6efe380dd60e03039fb1f8c4fd8baa1d6adce8fa
Mattheos Drivas <110428845+mdrivas@users.noreply.github.com>
2024-10-23 15:54:10 -0700

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   Navigate
@@ -18,11 +18,11 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [load, upadateLoad] = useState(true);
+  const [load, updateLoad] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      upadateLoad(false);
+      updateLoad(false);
     }, 1200);
 
     return () => clearTimeout(timer);
